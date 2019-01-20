f9927d663dc51e61bde95023fc67346b2a7b9e8c
Sufyan <sufyan.ash@ucp.edu.pk>
2019-01-20 16:45:28 +0500

    client + server side validation

--- pages/login.php
+++ pages/login.php
@@ -1,9 +1,34 @@
 <?php include("../config.php");?>
 <?php include_once("../includes/header.php"); ?>
+
+<?php
+    require_once "../server/database_connection.php";
+    
+    if(isset($_POST['login'])){
+        $email = $_POST['email'];
+        $pass = $_POST['password'];
+
+        $sql = "SELECT  * FROM `users` WHERE user_email = '$email' AND  user_password = '$pass'";
+        $result = $conn->query($sql);
+
+        if($result->num_rows > 0)
+        {
+            header('Location: ../index.php'); 
+        }
+        else
+        {
+            echo '<script> 
+            document.getElementById("pass_error").innerHTML = "*Email and Password does not match";
+            </script>';
+
+        }
+    }
+?>
+
 <body style="height: 100vh; background: #F9F9F9;" class="d-flex justify-content-center align-items-center">
 
 <div class="container">
-    <form action="login.php" method="get">
+    <form action="login.php" onsubmit="return stringCheck()" method ="POST">
         <div class="col-sm-12 offset-md-2 col-md-8 offset-lg-3 col-lg-6">
             <div class="container">
                 <div class="logo-container text-center mb-4">
@@ -14,20 +39,22 @@
                         <h2 class="text-center"> Log<span>in</span>  </h2>
                     </div>
                     <div class="card-body">
-                        <div class="input-group mb-2">
+						<div class="input-group mb-2">
                             <div class="input-group-prepend">
                                 <div class="input-group-text"><i class="fas fa-envelope"></i></div>
                             </div>
-                            <input class="form-control" type="text" name="email" placeholder ="Enter your Email" required>
+                            <input class="form-control" id= "email" type="text" name="email" placeholder ="Enter your Email" required>
                         </div>
+						<span id= "email_error" class="text-danger"> </span> 
                         <div class="input-group mb-2">
                             <div class="input-group-prepend">
                                 <div class="input-group-text"><i class="fas fa-key"></i></div>
                             </div>
-                            <input class="form-control" type="password" name="password" placeholder="Password" required>
+                            <input class="form-control" id= "password" type="password" name="password" placeholder="Password" required>
                         </div>
+						<span id= "pass_error" class="text-danger"> </span> 
                         <div class="d-flex justify-content-between align-items-center mb-2">
-                            <button type="submit" class="btn btn-md btn-dark px-4"> Login </button>
+                            <button type="submit" name= "login" class="btn btn-md btn-dark px-4"> Login </button>
                             <div class="mb-2"> <a href="<?php echo $domain.$root_folder."pages/forgot-password.php"; ?>"> <i class="fas fa-key"></i> Forgot your password?</a></div>                    
                         </div>
                     </div>
@@ -40,5 +67,6 @@
     </form>
 </div>
 
-
+<script src="../src/js/validationForm.js"></script>
+</body>
 <?php include_once("../includes/footer.php"); ?>
