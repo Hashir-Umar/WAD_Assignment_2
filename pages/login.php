96e2423b0ad0dd1dde959763d33c2c25aa7fd74c
ranaumair97 <44468043+ranaumair97@users.noreply.github.com>
2019-01-20 15:17:36 +0500

    updated design

--- pages/login.php
+++ pages/login.php
@@ -1,48 +1,44 @@
 <?php include("../config.php");?>
 <?php include_once("../includes/header.php"); ?>
-<body>
+<body style="height: 100vh; background: #F9F9F9;" class="d-flex justify-content-center align-items-center">
 
-    <nav class="navbar navbar-expand-md py-4">
-        <div class="wrapper">
-            <span class="logo ml-5">HOSTEL</span>
-        </div>
-    </nav>
-
-    <div class="container">
-        <div class="vertical-align">
-        <h1 class="text-center mb-4"> Log<span>in</span>  </h1>
-        <form action="#">
-            <div class="row">
-                <div class="col-sm-12 offset-md-2 col-md-8 offset-lg-3 col-lg-6">
-                    <div class="input-group mb-2">
-                        <div class="input-group-prepend">
-                            <div class="input-group-text"><i class="fas fa-envelope"></i></div>
+<div class="container">
+    <form action="login.php" method="get">
+        <div class="col-sm-12 offset-md-2 col-md-8 offset-lg-3 col-lg-6">
+            <div class="container">
+                <div class="logo-container text-center mb-4">
+                    <div class="logo d-inline font-20 margin-left-70"> <a href="<?php echo $domain.$root_folder."index.php"; ?>">HOSTEL</a></div>
+                </div> 
+                <div style="background:#ECECEC;" class="card mb-2">
+                    <div class="card-header">
+                        <h2 class="text-center"> Log<span>in</span>  </h2>
+                    </div>
+                    <div class="card-body">
+                        <div class="input-group mb-2">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text"><i class="fas fa-envelope"></i></div>
+                            </div>
+                            <input class="form-control" type="text" name="email" placeholder ="Enter your Email" required>
                         </div>
-                        <input class="form-control" type="text" name="email" placeholder ="Enter your Email" required>
-                    </div>
-                    <div class="input-group mb-2">
-                        <div class="input-group-prepend">
-                            <div class="input-group-text"><i class="fas fa-key"></i></div>
+                        <div class="input-group mb-2">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text"><i class="fas fa-key"></i></div>
+                            </div>
+                            <input class="form-control" type="password" name="password" placeholder="Password" required>
                         </div>
-                    <input class="form-control" type="password" name="password" placeholder="Password" required>
+                        <div class="d-flex justify-content-between align-items-center mb-2">
+                            <button type="submit" class="btn btn-md btn-dark px-4"> Login </button>
+                            <div class="mb-2"> <a href="<?php echo $domain.$root_folder."pages/forgot-password.php"; ?>"> <i class="fas fa-key"></i> Forgot your password?</a></div>                    
+                        </div>
                     </div>
-                    <button id=btn type="submit" class="btn btn-block btn-outline-dark  mb-2"> Login </button>
-                    <div class="lables mb-2">
-                        <p> <a href="forget-password.php"> <i class="fas fa-key"></i> Forgot your password?</a></p>
-                        <p> <a href="signup.php"> <i class="fas fa-sign-in-alt"></i> Signup for free</a></p>
+                    <div class="card-footer">
+                        <div class="d-flex justify-content-center"> <a href="<?php echo $domain.$root_folder."pages/signup.php"; ?>"> <i class="fas fa-sign-in-alt"></i> Create a free Account</a></div>                  
                     </div>
-                    <a href="../index.php"><i class="fas fa-arrow-left"></i>&nbsp;&nbsp;Back to Home</a>
                 </div>
-            </div>
-        
-        </form>
-    </div>
+            </div>    
+        </div>
+    </form>
 </div>
 
-    <footer>
-        <div class='container text-center py-4'>
-            All Rights Reserved. <a href='#' class="text-muted"> hostel.info </a>  &copy; 2018
-        </div>
-    </footer>
 
 <?php include_once("../includes/footer.php"); ?>
--- pages/signup.php
+++ pages/signup.php
@@ -1,79 +1,73 @@
 ﻿<?php include("../config.php");?>
 <?php include_once("../includes/header.php"); ?>
-<body>
+<body style="height: 100vh; background: #F9F9F9;" class="d-flex justify-content-center align-items-center">
 
-    <nav class="navbar navbar-expand-md py-2 py-md-4">
-        <div class="wrapper">
-            <span class="logo ml-5">HOSTEL</span>
-        </div>
-    </nav>
-
-    <div class="container">
-        <h1 class="text-center my-4"> Sign<span>up </span></h1>
-         <form action="#">
-            <div class="row">
-                <div class="col-sm-12 offset-md-3 col-md-6 col-lg-6">
-                    <div class="input-group mb-1 mb-md-2">
-                        <div class="input-group-prepend">
-                            <div class="input-group-text"><i class="fas fa-user"></i></i></div>
+<div class="container">
+    <form action="#">
+        <div class="col-sm-12 offset-md-2 col-md-8 offset-lg-3 col-lg-6">
+            <div class="container">
+                <div class="logo-container text-center mb-4">
+                    <div class="logo d-inline font-20 margin-left-70"> <a href="<?php echo $domain.$root_folder."index.php"; ?>">HOSTEL</a></div>
+                </div> 
+                <div style="background:#ECECEC;" class="card mb-2">
+                    <div class="card-header">
+                        <h1 class="text-center"> Sign<span>up </span></h1>
+                    </div>
+                    <div class="card-body">
+                        <div class="input-group mb-1 mb-md-2">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text"><i class="fas fa-user"></i></div>
+                            </div>
+                            <input class="form-control" type="text" name="first_name" placeholder="first name" required>
                         </div>
-                        <input class="form-control" type="text" name="first_name" placeholder="first name" required>
-                    </div>
-                    <div class="input-group mb-1 mb-md-2">
-                        <div class="input-group-prepend">
-                            <div class="input-group-text"><i class="fas fa-user"></i></i></div>
+                        <div class="input-group mb-1 mb-md-2">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text"><i class="fas fa-user"></i></div>
+                            </div>
+                            <input class="form-control" type="text" name="last_name" placeholder="last name" required>
                         </div>
-                        <input class="form-control" type="text" name="last_name" placeholder="last name" required>
-                    </div>
-                    <div class="input-group mb-1 mb-md-2">
-                        <div class="input-group-prepend">
-                            <div class="input-group-text"><i class="fas fa-calendar-alt"></i></i></div>
+                        <div class="input-group mb-1 mb-md-2">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text"><i class="fa fa-mars"></i></div>
+                            </div>
+                            <select class="form-control" id="Gender" name="Gender" required>
+                                <option>Male</option>
+                                <option>Female</option>
+                            </select>
                         </div>
-                            <input class="form-control" type="text" name="birthday" placeholder="11/18/1999" required>
-                    </div>
-                    <div class="input-group mb-1 mb-md-2">
-                        <div class="input-group-prepend">
-                            <div class="input-group-text"><i class="fa fa-mars"></i></div>
+                        <div class="input-group mb-1 mb-md-2">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text"><i class="fas fa-envelope"></i></div>
+                            </div>
+                            <input class="form-control" type="text" name="email" placeholder ="Enter your Email" required>
                         </div>
-                        <select class="form-control" id="Gender" name="Gender" required>
-                            <option>Male</option>
-                            <option>Female</option>
-                        </select>
-                    </div>
-                    <div class="input-group mb-1 mb-md-2">
-                        <div class="input-group-prepend">
-                            <div class="input-group-text"><i class="fas fa-envelope"></i></div>
+                        <div class="input-group mb-1 mb-md-2">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text"><i class="fas fa-key"></i></div>
+                            </div>
+                            <input class="form-control" type="password" name="password" placeholder="Password" required>
                         </div>
-                        <input class="form-control" type="text" name="email" placeholder ="Enter your Email" required>
-                    </div>
-                    <div class="input-group mb-1 mb-md-2">
-                        <div class="input-group-prepend">
-                            <div class="input-group-text"><i class="fas fa-key"></i></div>
+                        <div class="input-group mb-1 mb-md-2">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text"><i class="fas fa-key"></i></div>
+                            </div>
+                            <input class="form-control" type="password" name="confirm_password" placeholder="Confirm your password" required>
                         </div>
-                        <input class="form-control" type="password" name="password" placeholder="Password" required>
+                        <div class="d-flex justify-content-between align-items-center mb-2">
+                            <button type="submit" class="btn btn-md btn-dark px-4"> Register </button>
+                            <div class="mb-2"> 
+                                <input type="checkbox" name="user_account_type" value="1"> Are you an owner of Hostels?<br>
+                            </div>                
+                        </div>      
                     </div>
-                    <div class="input-group mb-1 mb-md-2">
-                        <div class="input-group-prepend">
-                            <div class="input-group-text"><i class="fas fa-key"></i></div>
-                        </div>
-                        <input class="form-control" type="password" name="confirm_password" placeholder="Confirm your password" required>
+                    <div class="card-footer">
+                        <div class="d-flex justify-content-center"> <a href="<?php echo $domain.$root_folder."pages/login.php"; ?>"> <i class="fas fa-sign-in-alt"></i>Already Have An Account?</a></div> 
                     </div>
-                    <button id=btn type="submit" class="btn btn-block btn-outline-dark mb-1 mb-md-2"> Register </button>
-                    <div class="lables">
-                        <p> <a href="login.php"> <i class="fas fa-sign-in-alt"></i> Already Have An Account?</a></p>
-                    </div>
-                    <a href="../index.php"><i class="fas fa-arrow-left"></i>&nbsp;&nbsp;Back to Home</a>
                 </div>
-            </div> 
-         </form>
-     </div>
-
-     <footer>
-        <div class='container text-center py-2 py-md-4'>
-            All Rights Reserved. <a href='#' class="text-muted"> hostel.info </a>  &copy; 2018
+            </div>    
         </div>
-    </footer>
-    
+    </form>
+</div>
 
 
 <?php include_once("../includes/footer.php"); ?>
