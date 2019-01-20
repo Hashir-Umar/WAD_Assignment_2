fe31fbb09875f824226d81fd598b1b790e5e609e
Hashir Umar <32617270+Hashir-Umar@users.noreply.github.com>
2019-01-20 21:06:23 +0500

    added dummy data + created pending_hostel and pending_users table. close #16

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php include("config.php");?>
 <?php include_once("includes/header.php"); ?>
+<?php include_once("server/database_init.php"); ?>
 
 <body class="container-fluid"> 
 
--- server/database_init.php
+++ server/database_init.php
@@ -20,7 +20,44 @@
 			);';
 
 	if(!mysqli_query($conn, $sql))
-		die("users table creation failed");
+		die("Error description: " . mysqli_error($conn));
+	else 
+	{
+		$sql = "INSERT INTO `users`
+		( user_email, user_password, user_account_type )
+		VALUES
+		('user@test.com', '123', 1), 
+		('user1@test.com', '123', 1), 
+		('user2@test.com', '123', 1),
+		('owner@test.com', '123', 3);";
+
+		$result = mysqli_query($conn, $sql);
+		if(!$result) {
+			die("Error description: " . mysqli_error($conn));
+		} 
+	}
+	// table to store user pending users data
+	$sql = 'CREATE TABLE IF NOT EXISTS `pending_users` (
+		`user_id` int(11) NOT NULL AUTO_INCREMENT,
+		`user_email` varchar(30) NOT NULL,
+		`user_password` varchar(255) NOT NULL,
+		`user_account_type` TINYINT NOT NULL,
+		PRIMARY KEY (`user_id`)
+	  );';
+
+	if(!mysqli_query($conn, $sql))
+		die("Error description: " . mysqli_error($conn));
+	else 
+	{
+		$sql = "INSERT INTO `pending_users`
+		( user_email, user_password, user_account_type )
+		VALUES ('admin@test.com', '123', 2);";
+
+		$result = mysqli_query($conn, $sql);
+		if(!$result) {
+			die("Error description: " . mysqli_error($conn));
+		} 
+	}
 
 	//table to store hostel data
 	$sql = 'CREATE TABLE IF NOT EXISTS `hostels` (
@@ -37,7 +74,38 @@
 			  	FOREIGN KEY (`hostel_owner`) REFERENCES `users` (`user_id`)
 			);';
 	if(!mysqli_query($conn, $sql))
-		die("hostels table creation failed");
+		die("Error description: " . mysqli_error($conn));
+
+	//table to store hostel pending data
+	$sql = 'CREATE TABLE IF NOT EXISTS `pending_hostels` (
+		`hostel_id` int(11) NOT NULL AUTO_INCREMENT,
+		`hostel_name` varchar(60) NOT NULL,
+		`hostel_city` varchar(30) NOT NULL,
+		`hostel_address` varchar(100) NOT NULL,
+		`hostel_rooms` int(3) NOT NULL,
+		`hostel_extras` varchar(255) NOT NULL,
+		`hostel_owner` int(11) NOT NULL,
+		`hostel_rating` float NOT NULL,
+		`hostel_img` TEXT NOT NULL,
+			PRIMARY KEY (`hostel_id`),
+			FOREIGN KEY (`hostel_owner`) REFERENCES `users` (`user_id`)
+	  );';
+	if(!mysqli_query($conn, $sql))
+		die("Error description: " . mysqli_error($conn));
+	else
+	{
+		$sql = "INSERT INTO `pending_hostels`
+			( hostel_name, hostel_city, hostel_address, hostel_rooms, hostel_extras, hostel_owner, hostel_img )
+			VALUES
+			('hostel1', 'Lahore', 'Johar town 12', '12', 'AC, fridge', '2', 'src/hostel_images/1_1.jpg'), 
+			('hostel2', 'Islamabad', 'Johar town 92', '9', 'AC, fridge', '2', 'src/hostel_images/1_2.jpg'), 
+			('hostel3', 'Karachi', 'Johar town 102', '3', 'AC, fridge', '2', 'src/hostel_images/1_3.jpg');";
+
+			$result = mysqli_query($conn, $sql);
+			if(!$result) {
+				die("Error description: " . mysqli_error($conn));
+			} 
+	}
 
 	// table to store universities and their address
 	$sql = 'CREATE TABLE IF NOT EXISTS `universities` (
@@ -48,7 +116,7 @@
 			  	PRIMARY KEY (`university_id`)
 			);';
 	if(!mysqli_query($conn, $sql))
-		die("universities table creation failed");
+		die("Error description: " . mysqli_error($conn));
 
 	// table to store hostels nearest to universities
 	$sql = 'CREATE TABLE IF NOT EXISTS `hostels_universities` (
@@ -59,7 +127,7 @@
 			  	FOREIGN KEY (`hostel_id`) REFERENCES `hostels` (`hostel_id`)
 			);';
 	if(!mysqli_query($conn, $sql))
-		die("hostels_universities table creation failed");
+		die("Error description: " . mysqli_error($conn));
 	
 	// table to store the pictures of the hostels
 	$sql = 'CREATE TABLE IF NOT EXISTS `hostels_images` (
@@ -68,8 +136,7 @@
 			  	FOREIGN KEY (`hostel_id`) REFERENCES `hostels` (`hostel_id`)
 			);';
 	if(!mysqli_query($conn, $sql))
-		die("hostels_images table creation failed");
-
+		die("Error description: " . mysqli_error($conn));
+	
 	mysqli_close($conn);
-	echo "Database created successfully";
 ?>
