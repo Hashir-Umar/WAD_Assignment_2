14e5c9c9d35c3985e8578e8c83b204e688213350
Hashir Umar <32617270+Hashir-Umar@users.noreply.github.com>
2019-01-21 19:02:11 +0500

    Update logout.php

--- server/logout.php
+++ server/logout.php
@@ -1,8 +1,8 @@
 <?php
-    include_once("../includes/session_start.php");  
+    session_start();
     if(isset($_SESSION['user_id']) && isset($_SESSION['user_account_type']))
     {
         session_destroy();
-        header("Location: ../index.php");
     }
+    header("Location: ../index.php");
 ?>
