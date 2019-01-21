181a0798ac0335197ee3a78f05225760be45d80c
Wisaam <wisaam.arif.99@gmail.com>
2019-01-21 12:25:13 +0500

    Updated Admin Panel Design

--- pages/admin-panel.php
+++ pages/admin-panel.php
@@ -7,7 +7,7 @@
 
     <?php include("../includes/admin-panel-navbar.php");?>
 
-    <div class="container-fluid padding-20">
+    <div class="container">
 
         <!-- Modal -->
         <div class="modal fade" id="myModal" tabindex="-1" role="dialog">
@@ -64,24 +64,16 @@
 
         <!-- End error/success message -->
 
-        <div class = "row">
-            <div class="col-12 text-block font-20"> Pending Hostel Requests </div>
-            <div class="table-responsive">
-                <table class="col-12 table table-striped">
+        <div class = "row align-items-center">
+            <?php
+                $sql = "SELECT * FROM pending_hostels;";
+                $result = mysqli_query($conn, $sql);
+                $count = mysqli_num_rows($result);
+            ?>
+            <div class="col-12 text-block font-20"> Pending Hostel Requests (<?php echo "$count"; ?>)</div>
+        </div><hr>
 
                     <?php
-                        $sql = "SELECT * FROM pending_hostels;";
-                        $result = mysqli_query($conn, $sql);
-                        $count = mysqli_num_rows($result);
-                        echo "<thead><tr>
-                                <th> Name </th>
-                                <th> Address </th>
-                                <th> City </th>
-                                <th> Rooms </th>
-                                <th> Extras </th>
-                                <th> Owner </th>
-                                <th> Options </th>
-                              </tr></thead>";
                         for ($var = 0; $var < $count; $var++)
                         {
                             $assoc = mysqli_fetch_assoc($result);
@@ -92,49 +84,72 @@
                             $city = $assoc['hostel_city']; 
                             $rooms = $assoc['hostel_rooms'];
                             $extras = $assoc['hostel_extras'];
-                            echo "<tr>
-                                    <td> $name </td>
-                                    <td> $addr </td>
-                                    <td> $city </td>
-                                    <td> $rooms </td>
-                                    <td> $extras </td>
-                                    <td> $owner </td>
-                                    <td>
-                                    <button class='openDiag btn btn-danger btn-sm'
-                                        data-toggle='modal'
-                                        data-id='../server/admin-control.php?reject_hostel_req=$pendingID'
-                                        data-btn-type='btn-danger'
-                                        data-btn-text='Yes, Reject'
-                                        data-body='Are you sure you want to reject this request?'
-                                        data-title='Confirm Request Rejection'
-                                        data-target='#myModal'>
-                                            Reject
-                                        </button> &nbsp;&nbsp;
+                            $image = "../".$assoc['hostel_img'];
 
-                                    <button class='openDiag btn btn-success btn-sm'
-                                        data-toggle='modal'
-                                        data-id='../server/admin-control.php?accept_hostel_req=$pendingID'
-                                        data-btn-type='btn-success'
-                                        data-btn-text='Yes, Accept'
-                                        data-body='Are you sure you want to accept this request?'
-                                        data-title='Confirm Request Acception'
-                                        data-target='#myModal'>
-                                            Accept
-                                        </button></td>
+                            echo '<div class = "row align-items-center">';
+                            echo "<div class='text-center col-lg-3 col-md-4 col-12'><div class='image-holder'><img alt='$name' class='img-fluid' src='$image'></div><div class='font-18 margin-top-10'><b>$name</b></div></div>";
+                            echo '<table class="table table-striped table-sm col-lg-9 col-md-8 col-12">';
+                            echo "<tr>
+                                    <th> Address </td>
+                                    <td> $addr </td>
+                                    <th> Extras </td>
+                                    <td> $extras </td>
+                                  </tr>
+                                  <tr>
+                                    <th> City </td>
+                                    <td> $city </td>
+                                    <th> Owner </td>
+                                    <td> $owner </td>
+                                  </tr>
+                                  <tr>
+                                    <th> Room </td>
+                                    <td> $rooms </td>
+                                    <th> Options </th>
+                                    <td>
+                                        <div class='row'>
+                                            <button class='openDiag btn btn-danger btn-sm'
+                                                data-toggle='modal'
+                                                data-id='../server/admin-control.php?reject_hostel_req=$pendingID'
+                                                data-btn-type='btn-danger'
+                                                data-btn-text='Yes, Reject'
+                                                data-body='Are you sure you want to reject this request?'
+                                                data-title='Confirm Request Rejection'
+                                                data-target='#myModal'>
+                                                    Reject
+                                                </button> &nbsp;&nbsp;
+
+                                            <button class='openDiag btn btn-success btn-sm'
+                                                data-toggle='modal'
+                                                data-id='../server/admin-control.php?accept_hostel_req=$pendingID'
+                                                data-btn-type='btn-success'
+                                                data-btn-text='Yes, Accept'
+                                                data-body='Are you sure you want to accept this request?'
+                                                data-title='Confirm Request Acception'
+                                                data-target='#myModal'>
+                                                    Accept
+                                                </button>
+                                        </div>
+                                    </td>
                                   </tr>";
+                            echo '</table>';
+                            echo '</div><br><hr><br>';
                         }
                     ?>
 
-                </table>
-            </div>
+        <div class = "row align-items-center">
+            <?php
+                $sql = "SELECT * FROM pending_users;";
+                $result = mysqli_query($conn, $sql);
+                $count = mysqli_num_rows($result);
+            ?>
+            <div class="col-12 text-block font-20"> Pending Hostel Owner Account Requests (<?php echo "$count"; ?>) </div>
 
-            <div class="col-12 text-block font-20"> Pending Hostel Owner Account Requests </div>
+        </div>
+
+        <div class = "row align-items-center">
             <table class="col-12 table table-striped">
 
                 <?php
-                    $sql = "SELECT * FROM pending_users;";
-                    $result = mysqli_query($conn, $sql);
-                    $count = mysqli_num_rows($result);
                     echo "<thead><tr>
                             <th><center> Email </center></th>
                             <th><center> Options </center></th>
