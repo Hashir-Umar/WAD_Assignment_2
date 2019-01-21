4cbb84dc6de0b4de79cae650553620066a6422df
Wisaam <wisaam.arif.99@gmail.com>
2019-01-21 17:31:56 +0500

    Added Script to footer + updated navbar style

--- includes/navbar.php
+++ includes/navbar.php
@@ -2,7 +2,7 @@
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="wrapper">
+    <div class="wrapper-navbar">
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <a href="<?php echo $domain.$root_folder ?>"><span class="logo d-none d-md-inline">HOSTEL</span></a>
         <ul class="navbar-nav mx-auto">
